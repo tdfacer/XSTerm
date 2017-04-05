eb9d6b8fe69d7a9f4c84e3e2a467a47f7235dd24
Trevor Facer <trevorf@ifit.com>
2017-04-04 23:39:35 -0600

    * adding enter button support
    * removing labels

--- XSTerm/xterm.js/demo/main.js
+++ XSTerm/xterm.js/demo/main.js
@@ -77,8 +77,6 @@ function runTerminal() {
 }
 
 function runSomething (text, caseInsensitive, reg) {
-//function runSomething (text) {
-	//var command = "adb logcat | grep " + text + "\r";
 	var command = "adb logcat";
 if (text.length > 0){
 	command += " | grep --color=auto ";
@@ -88,49 +86,25 @@ if (text.length > 0){
 		command += "-E ";
 	}
 	command += "'" + text + "'";
- //| grep " + text + "\r";
-} 	//if (reg) {
-	//	command += "-E ";
-	//} else if (case) {
-	//	command += "-i ";
-	//}
-
-	//command += text + "\r";
+}
 	command += "\r";
 
-	//var command = "adb logcat | grep " + text + "\r";
-	//term.send("\3\r");
 	term.send("\3");
 	term.send("clear\r");
 	term.send(command);
-	//term.send ("ls\r");
-	//term.send(something);
 }
 
 function myFunction() {
-	//runSomething(document.getElementById("search").value);
-	//document.getElementById("label1").innerHTML = document.getElementById("search").value;
-	//document.getElementById("label2").innerHTML = document.getElementById("regex").checked;
-	//document.getElementById("label3").innerHTML = document.getElementById("case").checked;
-
-
 	var w = document.getElementById("search").value;
 	var x = document.getElementById("case").checked;
 	var y = document.getElementById("regex").checked;
 
 	runSomething(w, x, y);
-	//runSomething(w, true, false);
 
-	if (x) {
-		document.getElementById("label2").innerHTML = "pizza";
-	}
-	if (y) {
-		document.getElementById("label3").innerHTML = "pasta";
-	}
+}
 
-
-	//var text = document.getElementById("search").value;
-	//var regex = document.getElementById("regex").checked;
-	//var case = document.getElementById("case").checked;
-	//runSomething(text, regex, case);
+document.getElementById('search').onkeydown = function(event) {
+    if (event.keyCode == 13) {
+        myFunction();
+    }
 }
